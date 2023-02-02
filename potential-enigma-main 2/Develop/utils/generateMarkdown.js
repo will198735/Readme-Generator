f9e22a923bbff872957d177d7d72bc669cca84b0
will198735 <recinoswilfredo@gmail.com>
2023-02-01 21:31:18 -0800

    added functions to the generatemarkdown file

--- potential-enigma-main 2/Develop/utils/generateMarkdown.js
+++ potential-enigma-main 2/Develop/utils/generateMarkdown.js
@@ -1,10 +1,25 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const badge = {
+    'MIT': `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`,
+    'APACHE 2.0': `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`,
+    'GPL 3.0': `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`,
+   }
+   return badge[license]
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const link = {
+    'MIT': `https://opensource.org/lincenses/MIT`,
+    'APACHE 2.0': `https://opensource.org/licenses/Apache-2.0`,
+    'GPL 3.0': `https://www.gnu.org/licenses/gpl-3.0`,
+  }
+  return link[license]
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -13,6 +28,38 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ${renderLicenseBadge(data['license'])}
+  
+  
+  
+  ## Description ${data.description}
+  
+  
+  
+  ## Tablet of content ${data.content}
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Credits](#credits)
+  
+  
+  
+  
+  ## Intalletion ${data.installation}
+  
+  
+  
+  ## Usage${data.usege}
+  
+  
+  
+  ## Credits ${data.credits}
+  
+  
+  
+  
+  ## License ${data.license}
 
 `;
 }
